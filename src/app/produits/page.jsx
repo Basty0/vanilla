bc45605eabc22053a31449613ae33dfd0b5e52c0
Basty0 <126388152+Basty0@users.noreply.github.com>
2024-09-29 08:43:21 +0300

    Quelleque ajustement

--- src/app/produits/page.jsx
+++ src/app/produits/page.jsx
@@ -114,12 +114,12 @@ const Page = () => {
   };
 
   return (
-    <div className="mx-auto flex flex-col items-center md:max-w-[90%]  mt-16 ">
+    <div className="mx-auto flex flex-col items-center   mt-16 ">
       <h1 className="text-2xl font-bold">Produits</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 bg-white/60 backdrop-blur-xl p-4 rounded-xl">
         {produitsData.map((produit) => (
           <div className="relative p-2" key={produit.id}>
-            <Card className="border-none shadow-none rounded-xl hover:shadow-xl transition-all duration-300 w-[300px]">
+            <Card className="border-none shadow-none rounded-xl hover:shadow-xl transition-all duration-300 w-[350px]">
               <CardHeader>
                 <CardTitle>{produit.nom}</CardTitle>
                 <CardDescription>{produit.description}</CardDescription>
